974f7c60f391fbc484d4e04aa21b5669170058ae
Emma <>
2024-01-17 09:22:49 +0800

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firesdk';
+  constructor(private firebaseApp: FirebaseApp) {}
+
+  ngOnInit() {
+    // const messaging = getMessaging(this.firebaseApp);
+    // from(
+    //   getToken(messaging, {
+    //     vapidKey:
+    //       'BJD7XKX8M2cXsZd7tNwR98X0z5TKGVeHg0J8HLlwRfxSw8dqoifUOm00EbtZYCSJxNIQ89fYYj83Rbpfy7TYvH8',
+    //   })
+    // )
+    //   .pipe(take(1))
+    //   .subscribe((res)=>{
+    //     console.log(222,res)
+    //   });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { provideFirebaseApp,initializeApp } from '@angular/fire/app';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyAm_zoqGlX07ABxPAL0hHFf7YTvYm1Ou1U',
+        authDomain: 'fire-demo-a766b.firebaseapp.com',
+        projectId: 'fire-demo-a766b',
+        storageBucket: 'fire-demo-a766b.appspot.com',
+        messagingSenderId: '205007761014',
+        appId: '1:205007761014:web:5deca532d48f2ac78d3957',
+      })
+    ),
+    provideMessaging(() => getMessaging()),
   ],
   providers: [],
   bootstrap: [AppComponent]
