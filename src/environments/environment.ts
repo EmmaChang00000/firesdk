dc38aac84e0d388cb7068d06e8c0173c93626efb
Rex <rexliu@star-link.tech>
2024-01-21 01:12:13 +0800

    trace

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   publicKey:
     'BJD7XKX8M2cXsZd7tNwR98X0z5TKGVeHg0J8HLlwRfxSw8dqoifUOm00EbtZYCSJxNIQ89fYYj83Rbpfy7TYvH8',
 };
